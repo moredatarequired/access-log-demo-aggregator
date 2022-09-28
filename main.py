14cd67e43f84ea3a63283d0a9ed29764c199d175
Hugh Wimberly <hugh.wimberly@gmail.com>
2022-09-28 15:04:24 -0600

    Use (mounted) src and dest data volumes

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Check check, Docker check")
+import asyncio
+
+from access_logs.group_and_count import aggregate_from_dir
+
+if __name__ == "__main__":
+    asyncio.run(aggregate_from_dir("/data/src", "/data/dest"))
